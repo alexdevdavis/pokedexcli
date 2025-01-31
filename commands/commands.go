38b5b45e55742c75f24ecdc95b390486d6a96994
alexdevdavis <alexdevdavis@gmail.com>
2025-01-31 21:31:51 +0000

    feat: add a cli commands repository, readable with 'help'

---
+++ commands/commands.go
@@ -0,0 +1,61 @@
+package commands
+
+import (
+	"fmt"
+	"os"
+)
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func ExecuteCommand(command string) error {
+	cmd, exists := getCommands()[command]
+	if !exists {
+		return fmt.Errorf("Unknown command")
+	}
+	err := cmd.callback()
+	return err
+}
+
+var commandsRegistry map[string]cliCommand
+
+func init() {
+	commandsRegistry = map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
+}
+func getCommands() map[string]cliCommand {
+
+	return commandsRegistry
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Printf("Usage:\n\n")
+	commands := getCommands()
+	for _, v := range commands {
+		fmt.Printf("%s: %s\n", v.name, v.description)
+	}
+	fmt.Println()
+	return nil
+}
+
+func commandExit() error {
+	fmt.Print("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+
+}
--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	"github.com/alexdevdavis/pokedexcli/input"
+	"github.com/alexdevdavis/pokedexcli/commands"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		if scanner.Scan() {
+			dirtyLine := scanner.Text()
+			line := input.CleanInput(dirtyLine)
+			err := commands.ExecuteCommand(line[0])
+			if err != nil {
+				fmt.Printf("Error: %v\n", err)
+			}
+		}
+	}
 }
